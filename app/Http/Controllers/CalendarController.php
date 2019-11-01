38201d018ea94ad2ceb5bc9f86bdc23b74718033
Frank Garcia <garcia1901l@gmail.com>
2019-10-31 21:00:40 -0500

    Google calendar en laravel

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Calendar;
+use Illuminate\Http\Request;
+use Spatie\GoogleCalendar\Event;
+use Carbon\Carbon;
+
+class CalendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $event = new Event;
+
+        $event->name = 'A new full day event 5';
+        $event->startDate = Carbon::now();
+        $event->endDate = Carbon::now()->addDay();
+        $event->save();
+
+        $events = Event::get();
+
+        dd($events);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Calendar $calendar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Calendar $calendar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Calendar $calendar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Calendar  $calendar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Calendar $calendar)
+    {
+        //
+    }
+}
